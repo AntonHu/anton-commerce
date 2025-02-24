6244c599899bf74fe2797c24927d79ab8829968d
安东开煮了 <504134526@qq.com>
2025-02-25 04:13:49 +0800

    docs(global): 全局文档变量命名改造

--- src/ui/json-ld.tsx
+++ src/ui/json-ld.tsx
@@ -51,11 +51,11 @@ export const accountToWebsiteJsonLd = ({
 	return {
 		"@context": "https://schema.org",
 		"@type": "WebSite",
-		name: account?.business_profile?.name ?? "Your Next Store",
-		url: account?.business_profile?.url ?? "https://yournextstore.com",
+		name: account?.business_profile?.name ?? "Anton Commerce",
+		url: account?.business_profile?.url ?? "https://github.com/AntonHu",
 		mainEntityOfPage: {
 			"@type": "WebPage",
-			url: account?.business_profile?.url ?? "https://yournextstore.com",
+			url: account?.business_profile?.url ?? "https://github.com/AntonHu",
 		},
 		...(logoUrl && {
 			image: {
@@ -65,8 +65,8 @@ export const accountToWebsiteJsonLd = ({
 		}),
 		publisher: {
 			"@type": "Organization",
-			name: account?.business_profile?.name ?? "Your Next Store",
-			url: account?.business_profile?.url ?? "https://yournextstore.com",
+			name: account?.business_profile?.name ?? "Anton Commerce",
+			url: account?.business_profile?.url ?? "https://github.com/AntonHu",
 		},
 	};
 };
